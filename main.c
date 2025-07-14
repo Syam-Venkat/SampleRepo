6e2d364f3b4ef123b7289e516f40cf0b1cb1b14b
Laxmi_Srinivas <syamvenkat.mulla@gmail.com>
2025-07-14 18:58:43 +0530

    After Some changes

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
     printf("Hello Dear, Welcome to the GitHub Session\n");
+    int max,smax;
+    int *ptr = (int*)malloc(5*sizeof(int));
+    if(ptr == NULL)
+    {
+        return 0;
+    }
+    for(int i=0;i<5;i++)
+    {
+        ptr[i] = i+1;
+    }
+    max = smax = ptr[0];
+    for(int i=0;i<5;i++)
+    {
+        if(ptr[i]>max)max = ptr[i];
+        else if(ptr[i]>smax && ptr[i]<max)smax=ptr[i];
+    }
+    printf("Maximum:%d\nSecond Maximum:%d\n",max,smax);
     return 0;
 }
